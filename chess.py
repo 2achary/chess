763f4f7a28585661e2d5e6d4d88e9c2c6d061e56
Zach Swift <zachary.swift@healthways.com>
2015-11-15 21:05:13 -0600

    add letter lookup

--- chess.py
+++ chess.py
@@ -1,9 +1,5 @@
 class Board:
 
-    piece_lookup = {
-        'r': 'rook'
-    }
-
     positions = {
         'r': [(0, 0), (0, 7)],
         'p': [(1, i) for i in range(8)],
@@ -22,6 +18,7 @@ class Board:
     def __init__(self):
         self.abc = 'a b c d e f g h'.split()
         self.board = [['_' for i in range(8)] for num in range(8)]
+        self.letter_lookup = dict(zip(self.abc, range(8)))
 
     def set_position(self, coordinates, piece):
         r, c = coordinates
@@ -65,6 +62,7 @@ class Pawn:
 if __name__ == '__main__':
     board = Board()
     # print(board.board)
+    print(board.letter_lookup)
     board.initialize_board()
     # print(board.board)
     board.print_board()
