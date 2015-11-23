36cfd47aedd8505d2e6d96f78a3508593dc7e854
Zach Swift <zachary.swift@healthways.com>
2015-11-22 21:12:08 -0600

    implement knight behavior

--- chess.py
+++ chess.py
@@ -47,6 +47,7 @@ class Board:
             print('{}    |'.format(n) + row_format.format(*row) + '|\n' + horizontal_line)
         print('\n' + letters)
 
+
 class Pawn:
 
     def __init__(self, color):
@@ -56,7 +57,7 @@ class Pawn:
         self.is_first_move = True
 
     def convert_to_coordinates(self, coordinate):
-        print('coordinate: {}'.format(coordinate))
+        # print('coordinate: {}'.format(coordinate))
         return int(coordinate[1]) - 1, self.letter_lookup[coordinate[0]]
 
     def get_moves(self, old, board):
@@ -90,43 +91,183 @@ class Pawn:
                 pass
 
             try:
-                if board[old[0] + 1][old[1] - 1] is not '_':
+                # allow pawn to capture diagonally if target is
+                # occupied by opponent piece
+                target = board[old[0] + 1][old[1] - 1]
+                if target.isalpha() and not target.islower():
                     moves.append((old[0] + 1, old[1] - 1))
             except IndexError:
                 pass
-        print('moves: {}'.format(moves))
+
+        if self.color == 'UPPER':
+            tmp = (old[0] - 1, old[1])
+
+            try:
+                # allow pawn to move forward one space if empty
+                if board[old[0] - 1][old[1]] == '_':
+                    moves.append(tmp)
+            except IndexError:
+                pass
+
+            try:
+                # allow pawn to move forward 2 spaces on its first move
+                if self.is_first_move:
+                    tmp = (old[0] - 2, old[1])
+                    if board[old[0] - 2][old[1]] == '_':
+                        moves.append(tmp)
+            except IndexError:
+                pass
+
+            try:
+                # allow pawn to capture diagonally if target is
+                # occupied by opponent piece
+                target = board[old[0] - 1][old[1] + 1]
+                if target.isalpha() and not target.islower():
+                    moves.append((old[0] - 1, old[1] + 1))
+            except IndexError:
+                pass
+
+            try:
+                # allow pawn to capture diagonally if target is
+                # occupied by opponent piece
+                target = board[old[0] - 1][old[1] - 1]
+                if target.isalpha() and not target.islower():
+                    moves.append((old[0] - 1, old[1] - 1))
+            except IndexError:
+                pass
+        # print('moves: {}'.format(moves))
         return moves
 
     def move(self, old, new, board):
         old = self.convert_to_coordinates(old)
         new = self.convert_to_coordinates(new)
         moves = self.get_moves(old, board)
-        print('new: {}'.format(new))
+        # print('new: {}'.format(new))
         if new in moves:
             # print('true')
             board[new[0]][new[1]] = 'p' if self.color == 'lower' else 'P'
             board[old[0]][old[1]] = '_'
             self.is_first_move = False
         else:
-            print('false')
+            raise MoveError('Invalid Move')
         return board
 
 
+class Knight:
+
+    def __init__(self, color):
+        self.color = color
+        self.abc = 'a b c d e f g h'.split()
+        self.letter_lookup = dict(zip(self.abc, range(8)))
+        self.is_first_move = True
+
+    def convert_to_coordinates(self, coordinate):
+        # print('coordinate: {}'.format(coordinate))
+        return int(coordinate[1]) - 1, self.letter_lookup[coordinate[0]]
+
+    def get_moves(self, old, board):
+        moves = []
+
+        for n in [-1, 1]:
+            for num in [-2, 2]:
+                # print n, num
+                # print num, n
+
+                tmp = (old[0] + n, old[1] + num)
+                try:
+                    target = board[old[0] + n][old[1] + num]
+                    if target == '_' or (target.isupper() is not self.color.isupper()):
+                        moves.append(tmp)
+                except IndexError:
+                    pass
+
+                tmp = (old[0] + num, old[1] + n)
+                try:
+                    target = board[old[0] + num][old[1] + n]
+                    if target == '_' or (target.isupper() is not self.color.isupper()):
+                        moves.append(tmp)
+                except IndexError:
+                    pass
+
+        return moves
+
+    def move(self, old, new, board):
+        old = self.convert_to_coordinates(old)
+        new = self.convert_to_coordinates(new)
+        moves = self.get_moves(old, board)
+        # print('new: {}'.format(new))
+        if new in moves:
+            # print('true')
+            board[new[0]][new[1]] = 'n' if self.color == 'lower' else 'N'
+            board[old[0]][old[1]] = '_'
+            self.is_first_move = False
+        else:
+            raise MoveError('Invalid Move')
+        return board
+
+
+class MoveError(Exception):
+    pass
+
+
 if __name__ == '__main__':
+    pieces = {
+        'p': Pawn,
+        'n': Knight
+    }
     board = Board()
     board.initialize_board()
     board.print_board()
+    turn = 'UPPER'
     while True:
-        old = input('select piece: ')
-        new = input('select new position: ')
-        old_coords = board.convert_to_coordinates(old)
-        new_coords = board.convert_to_coordinates(new)
-        print(old_coords)
-        name = board.board[old_coords[0]][old_coords[1]]
-        if name == 'p':
 
-            p = Pawn('lower')
-            board.board = p.move(old, new, board.board)
+        if turn == 'UPPER':
+
+            print("--- Upper's turn ---")
+            old = input('select piece: ')
+
+            old_coords = board.convert_to_coordinates(old)
+
+            # print(old_coords)
+            name = board.board[old_coords[0]][old_coords[1]]
+            try:
+                assert name.isupper()
+            except AssertionError:
+                print("Please choose your own piece")
+                continue
+            new = input('select new position: ')
+            new_coords = board.convert_to_coordinates(new)
+            current_piece = pieces[name.lower()]('UPPER')
+            try:
+                board.board = current_piece.move(old, new, board.board)
+            except MoveError:
+                print("invalid move! Please choose a legal move.")
+                continue
+            turn = 'lower'
             board.print_board()
-        else:
-            print(name)
+
+        if turn == 'lower':
+            print("--- Lower's turn ---")
+            old = input('select piece: ')
+
+            old_coords = board.convert_to_coordinates(old)
+
+            # print(old_coords)
+            name = board.board[old_coords[0]][old_coords[1]]
+            try:
+                assert name.islower()
+            except AssertionError:
+                print("Please choose your own piece")
+                continue
+
+            new = input('select new position: ')
+            new_coords = board.convert_to_coordinates(new)
+            current_piece = pieces[name.lower()]('lower')
+            try:
+                board.board = current_piece.move(old, new, board.board)
+            except MoveError:
+                print("invalid move! Please choose a legal move.")
+                continue
+            turn = 'UPPER'
+            board.print_board()
+
